356a93698563bcb5a790af736900a188783bd7c6
abel abraha <abrahaabel18@gmail.com>
2024-01-16 09:48:44 +0000

    loops

---
+++ day1/java.js
@@ -0,0 +1,21 @@
+let imagesArray = [
+    'https://images.unsplash.com/photo-1704409914802-4f2c5b037834?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 
+    'https://images.unsplash.com/photo-1705065277882-b0604ec13dca?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1682685797439-a05dd915cee9?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1682687980115-a37b56ea7271?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1483197452165-7abc4b248905?q=80&w=1336&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1705200225027-37f2371df0e0?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1705179910410-023d32f4613b?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+]
+
+// this function takes an array as an argument and loops through it creating an image tag for each of the elements in the array and appending it to the document.body. 
+function displayImages(arr) {
+    for (let i = 0; i < imagesArray.length; i++) {
+        let img = document.createElement('img')
+        img.src = arr[i] // imagesArray[i]
+        document.body.appendChild(img)
+    }
+}
+
+
+displayImages(imagesArray)
